1465d5ec84d57c753c7d0b8ccdac9919462301a7
gasgit <g00305555@gmit.ie>
2016-11-03 23:20:11 +0000

    update this time

--- mongo.js
+++ mongo.js
@@ -8,7 +8,7 @@ var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/GLEN_TEST';
 
 
-// 
+// open connection, message when good
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server.");
@@ -22,21 +22,23 @@ var user = { "name": "sean", "id": 118, "height": 185, "eye_col": "yellow" }
 var user2 = { "name": "julie", "id": 117, "height": 158, "eye_col": "blue" }
 var user3 = { "name": "fred", "id": 11233, "height": 160, "eye_col": "white" }
 
-var insertDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('mycollection');
-    // Insert some documents
-    collection.insertMany([
-        user, user2, user3
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-}
 
+// create insertDocuments function and callback
+var insertDocuments = function(db, callback) {
+        // get the documents collection
+        var collection = db.collection('mycollection');
+        // insert some documents( many or one as an array, check results)
+        collection.insertMany([
+            user, user2, user3
+        ], function(err, result) {
+            assert.equal(err, null);
+            assert.equal(3, result.result.n);
+            assert.equal(3, result.ops.length);
+            console.log("Inserted 3 documents into the collection");
+            callback(result);
+        });
+    }
+    // 
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     insertDocuments(db, function() {
@@ -45,24 +47,24 @@ MongoClient.connect(url, function(err, db) {
 });
 
 var findDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('mycollection');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("------------   All Docs  --------------------------")
-        console.log("\n")
-        console.log("Found the following records");
-        console.log("\n")
-        console.log(docs)
-        console.log("\n")
-        console.log("------------   End All   ---------------------------")
-        console.log("\n")
+        // get the documents collection
+        var collection = db.collection('mycollection');
+        // find some documents and store in an array
+        collection.find({}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            console.log("------------   All Docs  --------------------------")
+            console.log("\n")
+            console.log("Found the following records");
+            console.log("\n")
+            console.log(docs)
+            console.log("\n")
+            console.log("------------   End All   ---------------------------")
+            console.log("\n")
 
-        callback(docs);
-    });
-}
-
+            callback(docs);
+        });
+    }
+    // open connection and call findDocuments
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     findDocuments(db, function() {
@@ -72,24 +74,24 @@ MongoClient.connect(url, function(err, db) {
 
 
 var findDocumentsFilter = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('mycollection');
-    // Find some documents
-    collection.find({ 'name': "fred" }).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("-------------  Begin Filter -------------------------")
-        console.log("\n")
-        console.log("Found the following records by filter!!");
-        console.log("\n")
-        console.log(docs);
-        console.log("\n")
-        console.log("-------------  End  Filter  -------------------------")
-        console.log("\n")
+        // get the documents collection 
+        var collection = db.collection('mycollection');
+        // find some documents by filter and store in an array
+        collection.find({ 'name': "fred" }).toArray(function(err, docs) {
+            assert.equal(err, null);
+            console.log("-------------  Begin Filter -------------------------")
+            console.log("\n")
+            console.log("Found the following records by filter!!");
+            console.log("\n")
+            console.log(docs);
+            console.log("\n")
+            console.log("-------------  End  Filter  -------------------------")
+            console.log("\n")
 
-        callback(docs);
-    });
-}
-
+            callback(docs);
+        });
+    }
+    // open connection and call findDocumentsFilter
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     findDocumentsFilter(db, function() {
